fdf8384747332ac78266e4138dc510c88c64b946
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2023-12-01 20:13:16 +0200

    sss

--- .github/workflows/file.js
+++ .github/workflows/file.js
@@ -4,20 +4,19 @@ const fs = require('fs');
 const executeCommand = (command) =>
   execSync(command, { encoding: 'utf-8' }).trim()
 
-const getCMSChanges = () => {
+const getCMSChanges = async () => {
   const addedLines = executeCommand(
     `git diff ${process.env.GITHUB_EVENT_BEFORE}..${process.env.GITHUB_SHA}`
   )
   if (addedLines) {
-  fs.writeFile('issue-template.md', addedLines, (err) => {
-  if (err) {
-    console.error('Error writing to file:', err);
-  } else {
-    console.log('File written successfully');
+    try {
+        await fs.writeFile('issue-template.md', addedLines)
+        console.log('file successed write')
+    } catch(e) {
+      console.log(`${e}`)
+    }
   }
-});
-
-  }
+  process.exit(0)
 }
 
 module.exports = {
