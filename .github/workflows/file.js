4f9204349a564eabf1aaedd6114b4158a0b013bc
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2023-12-01 20:07:10 +0200

    sss

--- .github/workflows/file.js
+++ .github/workflows/file.js
@@ -1,15 +1,16 @@
 const { execSync } = require('child_process')
+const fs = require('fs');
 
 const executeCommand = (command) =>
   execSync(command, { encoding: 'utf-8' }).trim()
 
-const getCMSChanges = async () => {
+const getCMSChanges = () => {
   const addedLines = executeCommand(
     `git diff ${process.env.GITHUB_EVENT_BEFORE}..${process.env.GITHUB_SHA}`
   )
   if (addedLines) {
     try {
-        await fs.writeFile('issue-template.md', addedLines)
+        fs.writeFile('issue-template.md', addedLines)
         console.log('file successed write')
     } catch(e) {
       console.log(`${e}`)
