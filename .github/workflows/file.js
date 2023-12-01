8104da9efe1e9129967b4c2df4c0c03aa294a5f3
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2023-12-01 20:03:15 +0200

    aaa

--- .github/workflows/file.js
+++ .github/workflows/file.js
@@ -7,16 +7,16 @@ const getCMSChanges = async () => {
   const addedLines = executeCommand(
     `git diff ${process.env.GITHUB_EVENT_BEFORE}..${process.env.GITHUB_SHA} -- CMS/internationalisation_de_DE.json`
   )
-//   if (addedLines) {
-//     try {
-//         await fs.writeFile('issue-template.md', addedLines)
-//         console.log('file successed write')
-//     } catch {
-//       const errorMessage = 'An Error occured while parsing the json files'
-//       console.log(`::set-output name=result::${errorMessage}`)
-//     }
-//   }
-//   process.exit(0)
+  if (addedLines) {
+    try {
+        await fs.writeFile('issue-template.md', addedLines)
+        console.log('file successed write')
+    } catch {
+      const errorMessage = 'An Error occured while parsing the json files'
+      console.log(`::set-output name=result::${errorMessage}`)
+    }
+  }
+  process.exit(0)
 }
 
 module.exports = {
