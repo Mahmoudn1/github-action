4cf766acdff980951315cf4d6cc3a5155fa0855d
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2023-12-01 20:27:07 +0200

    dd

--- .github/workflows/file.js
+++ .github/workflows/file.js
@@ -6,7 +6,7 @@ const executeCommand = (command) =>
 
 const getCMSChanges = () => {
   const addedLines = executeCommand(
-    `git diff ${process.env.GITHUB_EVENT_BEFORE}..${process.env.GITHUB_SHA}`
+    `git diff ${process.env.GITHUB_EVENT_BEFORE}..${process.env.GITHUB_SHA} -- CMS/internationalisation_de_DE.json`
   )
   if (addedLines) {
     try {
